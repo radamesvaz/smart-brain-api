0fa92ce7256c877a0da8a3e079ebcc98915026cf
Radames Vaz <rgvtovar@hotmail.com>
2020-01-14 12:16:02 -0400

    still fixin

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-    res.send(database);
+    res.send('it is working!');
 })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
